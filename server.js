cb5bdd3b3d017d963667efb32175a53767a71916
uehr <uehara.yudai@gmail.com>
2020-10-09 02:46:43 +0900

    add: リマインダ/ヘルプ機能

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require('http');
 const querystring = require('querystring');
 const discord = require('discord.js');
-const speech = require('./lib/speech.js');
 const client = new discord.Client();
+const config = require("./config.json")
+const command = require("./lib/command.js")
+const speech = new (require('./lib/speech.js'))(client)
+const reminder = new (require("./lib/reminder"))(client)
+const cronCheck = require('cron-validator');
+
+reminder.syncDB()
 
 http.createServer(function(req, res){
   if (req.method == 'POST'){
@@ -49,11 +55,70 @@ client.on('message', message =>{
     speech.msg(message.channel.id, text);
     return;
   }
+
+  // リマインダー登録
+  command.ifStartWith(message.content, config.reminder_create_prefix, async args => {
+    if(args.length <= 1) throw "Invalid args."
+
+    const cron = args[0].replace(/-/g, " ")
+    const text = args.slice(1).join(" ")
+    const is_valid_cron = await cronCheck.isValidCron(cron)
+
+    if(is_valid_cron){
+        await reminder.add(message.channel.id, cron, text)
+        await speech.reply(message, config.reminder_create_success_message)
+    } else throw "Invalid cron syntax."
+  }).catch(err => { erorrHandler(message, err) })
+
+  // リマインダー取得
+  command.ifStartWith(message.content, config.reminder_get_prefix, async args => {
+    await speech.msg(message.channel.id, config.reminder_get_result_message)
+
+    const reminders = await reminder.get()
+    speech.reply(message, {embed: {
+        color: 0x00e191,
+        description: JSON.stringify(reminders, null, "　")
+    }})
+  }).catch(err => { erorrHandler(message, err) })
+
+  // リマインダー削除
+  command.ifStartWith(message.content, config.reminder_delete_prefix, async args => {
+    const id = args[0]
+    const deleted_count = await reminder.delete(id)
+    return speech.reply(message, deleted_count >= 1 ? config.reminder_delete_success_message : config.reject_message)
+  }).catch(err => { erorrHandler(message, err) })
+
+  // ヘルプ
+  command.ifStartWith(message.content, config.help_prefix, async args => {
+    return speech.reply(message, {embed: {
+        color: 0x00e191,
+        description: config.help_message.join("\n")
+    }})
+  }).catch(err => { erorrHandler(message, err) })
 });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined){
  console.log('DISCORD_BOT_TOKENが設定されていません。');
  process.exit(0);
+}
+
+function erorrHandler(message, err) {
+  console.error(err)
+
+  switch(err) {
+    case "Invalid args.":
+      speech.msg(message.channel.id, config.invalid_arg_message)
+      break
+    case "Invalid cron syntax.":
+      speech.msg(message.channel.id, config.invalid_cron_syntax_message)
+      break
+    default:
+      speech.msg(message.channel.id, config.error_message)
+      speech.reply(message, {embed: {
+          color: 0xff0000,
+          description: err.toString()
+      }})
+  }
 }
 
 client.login( process.env.DISCORD_BOT_TOKEN );
